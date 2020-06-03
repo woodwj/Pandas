fb99b54d06e9c226b9a4054500867eb69de78413
woodwj-Desktop <willwood808@gmail.com>
2020-06-03 12:43:03 +0100

    Task 7 - Complete

--- exImports.py
+++ exImports.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
+def colsLower(df):
+    cols = list(df.columns)
+    correct = [x.lower() for x in cols]
+    replace = {}
+    for index in range(0,len(correct)):
+        replace[cols[index]] = (correct[index])
+    shipPassangers.rename(columns = replace, inplace = True)
+
 shipPassangers = pd.read_csv("test.csv")
 
-print(shipPassangers.head(6))
-print(shipPassangers.tail(4))
+print(shipPassangers.columns)
+colsLower(shipPassangers)
+print(shipPassangers.columns)
+
