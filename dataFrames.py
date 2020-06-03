7356f416a9b8bf91d5741c5d4e15c3eb59dff17f
woodwj-Desktop <willwood808@gmail.com>
2020-06-03 10:43:04 +0100

    Task 3: Indexes - Complete

--- dataFrames.py
+++ dataFrames.py
@@ -3,11 +3,13 @@ pd.set_option('max_colwidth',10)
 pd.options.display.max_columns = 2
 pd.options.display.max_rows = 6
 
+indexi = ["Bob", "Karen", "James Bond", "Bleep", "Bloop", "Jesus Christ", "Gary", "Susan", "Harry", "Mom"]
+
 drinks = {
     'Diet Coke' : [0,0,2,1,1,4,5,6,2,3],
     'Orange Juice' : [0,4,1,1,0,0,2,3,0,1]
 }
 
-orders = pd.DataFrame(drinks)
+orders = pd.DataFrame(drinks, index=indexi)
 
 print(orders)
