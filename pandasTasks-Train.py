c6ef02ebc307680790d7421d15e6b4680419a2f3
woodwj-Desktop <willwood808@gmail.com>
2020-06-07 15:49:31 +0100

    Task 11

--- pandasTasks-Train.py
+++ pandasTasks-Train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def sortGroupBy(df, cols, Pr = False):
+def sortGroupCountBy(df, cols, Pr = False):
     groupCounts = df.groupby(cols)['Name'].count()
     if Pr:
         print("\n")
@@ -10,16 +10,22 @@ def sortGroupBy(df, cols, Pr = False):
         print("\n")
     else:   return(groupCounts)
 
+def sortGroupBy(df, cols, Pr = False):
+    group = df.sort_values(cols, ascending=True)
+    if Pr:
+        print("\n")
+        print("##########################")
+        print(group)
+        print("##########################")
+        print("\n")
+    else:   return(group)
+
 
 trainPassangers = pd.read_csv("train.csv")
-# Display number of passengers per Class
-sortGroupBy(trainPassangers, "Pclass", True)
-# Display number of passengers by Class and Sex
-sortGroupBy(trainPassangers, ["Pclass", "Sex"], True)
-# Display number of passengers by Class, Sex and Age
-sortGroupBy(trainPassangers, ["Pclass", "Sex", "Age"], True)
-# Display number of passengers in Class 1 by Sex and Age
-sortGroupBy(trainPassangers[trainPassangers["Pclass"] == 1], ["Sex","Age"], True)
-# Display bumber of all female passangers by Class
-sortGroupBy(trainPassangers[trainPassangers["Sex"] == "female"], "Pclass",True)
+# Display all male passengers grouped by Class
+sortGroupBy(trainPassangers[trainPassangers["Sex"]=="male"], "Pclass", True)
+# Display all passengers by grouped by Class and Sex
+sortGroupBy(trainPassangers, ["Pclass","Sex"], True)
+# Display all male passengers group by Embarked
+sortGroupBy(trainPassangers[trainPassangers["Sex"]=="male"], "Embarked", True)
 
